c1f49e1b22a004eadddd4be1f6a6dd175ee2f7bf
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2023-10-13 12:06:43 -0300

    Iniciando Spring Boot Java - Aula 01

---
+++ src/test/java/br/com/viniciusreis/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusreis.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
