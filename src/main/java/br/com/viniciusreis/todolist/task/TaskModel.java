80221ea8e5d925b4de073d63a40d1ffa8b49f079
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2023-10-14 22:14:18 -0300

    Creating the Task Entity (table)

---
+++ src/main/java/br/com/viniciusreis/todolist/task/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.viniciusreis.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+/**
+ * 
+ * ID
+ * Usuário (ID_USUARIO)
+ * Descrição
+ * Título
+ * Data de Início
+ * Data de Término
+ * Prioridade
+ * 
+ */
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+  
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private UUID idUser;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  
+  @Column(length = 50)
+  private String title;
+  private String description;
+  private String priority;
+
+
+}
